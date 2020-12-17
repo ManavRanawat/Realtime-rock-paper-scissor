b4fad2d83ad8ec97a00725d2faea4c5b60292975
Manav <manav.ranawat@spit.ac.in>
2020-12-17 15:40:12 +0530

    some changes in data.py also added comments

--- data.py
+++ data.py
@@ -10,8 +10,10 @@ import os
 import sys
 import numpy as np
 
+#make sure this variable has your current project directory
 current_dir = os.getcwd() + '\\'
 # print(current_dir)
+
 cap = cv2.VideoCapture(0)
 
 #label -> rock or paper or scissor
@@ -29,12 +31,15 @@ except FileExistsError:
 img_counter = int(sys.argv[2])
 max_img = sys.argv[3]
  
+
+#code for clicking the image from camera
 while(1):
     _,frame = cap.read()
     cv2.imshow('Collecting Data',frame[50:350,100:450])
+    # if ' '(space bar) is pressed->image is captured
     if cv2.waitKey(1) & 0xFF == ord(' '):
-        img_name = label+"{}.png".format(img_counter)
-        cv2.imwrite(os.path.join(SAVE_PATH,img_name), frame)
+        img_name = label+"{}.png".format(img_counter)   #naming each image uniquely
+        cv2.imwrite(os.path.join(SAVE_PATH,img_name), frame)    #storing the image in your directory
         print("{} written!".format(img_name))
         img_counter += 1
     if img_counter>= int(max_img) :
